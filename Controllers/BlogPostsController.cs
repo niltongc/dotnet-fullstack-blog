795ca598d5dd482b0a3244522c26d7920d263f71
nilton <ngcquimica@gmail.com>
2024-01-28 17:19:53 -0300

    save

--- Controllers/BlogPostsController.cs
+++ Controllers/BlogPostsController.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Core;
 using CodePulseAPI.Models.Domain;
 using CodePulseAPI.Models.DTO;
+using CodePulseAPI.Repositories.Implementation;
 using CodePulseAPI.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,12 @@ namespace CodePulseAPI.Controllers
     public class BlogPostsController : ControllerBase
     {
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ICategoryRepository categoryRepository;
 
-        public BlogPostsController(IBlogPostRepository blogPostRepository   )
+        public BlogPostsController(IBlogPostRepository blogPostRepository, ICategoryRepository categoryRepository)
         {
             this.blogPostRepository = blogPostRepository;
+            this.categoryRepository = categoryRepository;
         }
         [HttpPost]
         public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
@@ -30,8 +33,18 @@ namespace CodePulseAPI.Controllers
                 PublishedDate = request.PublishedDate,
                 ShortDescription = request.ShortDescription,
                 Title = request.Title,
-                UrlHandle = request.UrlHandle
+                UrlHandle = request.UrlHandle,
+                Categories = new List<Category>()
             };
+
+            foreach (var categoryGuid in request.Categories) 
+            {
+                var exisitingCategory = await categoryRepository.GetById(categoryGuid);
+                if (exisitingCategory is not null) 
+                { 
+                    blogPost.Categories.Add(exisitingCategory);
+                }
+            }
 
             _ = await blogPostRepository.CreateAsync(blogPost);
 
@@ -46,7 +59,13 @@ namespace CodePulseAPI.Controllers
                 PublishedDate = blogPost.PublishedDate,
                 ShortDescription = blogPost.ShortDescription,
                 Title = blogPost.Title,
-                UrlHandle = blogPost.UrlHandle
+                UrlHandle = blogPost.UrlHandle,
+                Categories = blogPost.Categories.Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlHandle = x.UrlHandle
+                }).ToList()
             };
             return Ok(response);
         }
