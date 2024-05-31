eea5033cd390a0b4e1f39a79c36bbcb151b4388b
nilton <ngcquimcia@gmail.com>
2024-05-31 17:56:56 -0300

    update blog

--- Repositories/Implementation/BlogPostRepository.cs
+++ Repositories/Implementation/BlogPostRepository.cs
@@ -22,7 +22,33 @@ namespace CodePulseAPI.Repositories.Implementation
 
         public async Task<IEnumerable<BlogPost>> GetAllAsync()
         {
-            return await dbContext.BlogPosts.ToListAsync();
+            return await dbContext.BlogPosts.Include(x => x.Categories).ToListAsync();
         }
+
+        public async Task<BlogPost?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.BlogPosts.Include(x => x.Categories).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        {
+            var existingBlogPost = await dbContext.BlogPosts.Include(x => x.Categories)
+                 .FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlogPost == null) 
+            {
+                return null;
+            }
+
+            dbContext.Entry(existingBlogPost).CurrentValues.SetValues(blogPost);
+
+            existingBlogPost.Categories = blogPost.Categories;
+
+            await dbContext.SaveChangesAsync();
+
+            return blogPost;
+        }
+
+
     }
 }
--- Repositories/Interface/IBlogPostRepository.cs
+++ Repositories/Interface/IBlogPostRepository.cs
@@ -7,5 +7,8 @@ namespace CodePulseAPI.Repositories.Interface
         Task<BlogPost> CreateAsync(BlogPost blogPost);
 
         Task<IEnumerable<BlogPost>> GetAllAsync();
+        Task<BlogPost?> GetByIdAsync(Guid id);
+
+        Task<BlogPost?> UpdateAsync(BlogPost blogPost);
     }
 }
